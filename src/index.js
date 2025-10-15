f328eb03f39486a2b33bb32903a721c5560a2d71
KagisoMaphoto <maphotokagiso84@gmail.com>
2025-10-15 22:19:26 +0200

    Added Basic search engine API

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/** @format */
+
+let searchFormElement = document.querySelector("#search-form");
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+searchFormElement.addEventListener("submit", handleSearchSubmit);
